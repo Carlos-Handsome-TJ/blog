0c3a2c2f17889d5a77703fb6198184b92f2d37fa
Jun <1356675512@qq.com>
2024-02-18 10:00:19 +0800

    build: 代码仓库初始化

---
+++ env.d.ts
@@ -0,0 +1,14 @@
+/// <reference types="vite/client" />
+declare module '*.vue' {
+  import { DefineComponent} from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+interface ImportMetaEnv {
+  readonly VITE_APP_PORT: number;
+  readonly VITE_BASE_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig  } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+export default () => {
+  return defineConfig({
+    plugins: [
+      vue(),
+      vueJsx()
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      proxy: {},
+      port: 3001,
+      open: true
+    }
+  });
+};
