e5d17a957959da5d7976f6d62095ce31fedb33f5
Jun <1356675512@qq.com>
2024-02-21 08:49:35 +0800

    ui: 搜索框宽度样式优化

--- src/views/home/components/search.vue
+++ src/views/home/components/search.vue
@@ -1,13 +1,40 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 
 const isShowSearchList = ref<boolean>(false)
 const searchVal = ref<string>('')
 const searchRecommend = ref<string>('推荐搜索词')
-const clearHistory = (e) => {
+const searchHistoryList = ref<(string)[]>([])
+const showCloseIcon = ref<boolean>(false)
+const clearHistory = (e: Event) => {
   e.preventDefault()
+  searchHistoryList.value = []
   localStorage.removeItem('searchHistoryList')
 }
+const search = (e: Event) => {
+  e.preventDefault()
+  if (searchVal.value) {
+    // 保存搜索历史
+    searchHistoryList.value.unshift(searchVal.value)
+    localStorage.setItem('searchHistoryList', JSON.stringify(searchHistoryList.value))
+  }
+}
+const showHistoryList = () => {
+  searchHistoryList.value = JSON.parse(localStorage.getItem('searchHistoryList') || '[]')
+}
+const removeTags = (e: Event, val: string) => {
+  e.preventDefault()
+  searchHistoryList.value = searchHistoryList.value.filter(item => item !== val)
+}
+const showCloseTag = () => {
+  showCloseIcon.value = true
+}
+const hideCloseTag = () => {
+  showCloseIcon.value = false
+}
+onMounted(() => {
+  showHistoryList()
+})
 </script>
 <template>
   <div class="search-container">
@@ -19,17 +46,32 @@ const clearHistory = (e) => {
         @focus="isShowSearchList = true"
         @blur="isShowSearchList = false"
         :placeholder="searchRecommend"
+        @keyup.enter="search"
         clearable
       >
       </el-input>
-      <el-button type="text">
-        <i-uiw-search></i-uiw-search>
+      <el-button type="text" @mousedown="search">
+        <i-uiw-search />
       </el-button>
     </div>
-    <div v-show="isShowSearchList" class="search-list">
-      <div flex justify-between w-full items-center p-2 box-border>
+    <div v-show="isShowSearchList" class="search-list" p-2 box-border>
+      <div flex justify-between w-full items-center>
         <span>搜索历史</span>
         <el-button type="text" @mousedown="clearHistory">清空</el-button>
+      </div>
+      <div class="search-list-container">
+        <el-tag
+          v-for="(item, index) in searchHistoryList"
+          :key="index"
+          relative
+          cursor-pointer
+          @mouseenter="showCloseTag"
+          @mouseleave="hideCloseTag"
+        >
+          <i-uiw-circle-close v-if="showCloseIcon" @mousedown="(e: Event) => removeTags(e, item)"
+                              class="search-tag-close" />
+          {{ item }}
+        </el-tag>
       </div>
     </div>
   </div>
@@ -41,6 +83,7 @@ const clearHistory = (e) => {
   height: 64px;
   display: flex;
   align-items: center;
+
   .search-input-area {
     width: 100%;
     display: flex;
@@ -50,6 +93,7 @@ const clearHistory = (e) => {
     border-radius: 5px;
     box-sizing: border-box;
     background: #e3e5e7;
+
     :deep(.el-input__wrapper) {
       background: #e3e5e7;
       box-shadow: none;
@@ -89,6 +133,20 @@ const clearHistory = (e) => {
     z-index: 1000;
     box-sizing: border-box;
     border-top: none;
+
+    .search-list-container {
+      display: flex;
+      gap: 10px;
+      flex-wrap: wrap;
+
+      .search-tag-close {
+        cursor: pointer;
+        position: absolute;
+        right: -5px;
+        top: -5px;
+        color: #e3e5e7;
+      }
+    }
   }
 }
 </style>
