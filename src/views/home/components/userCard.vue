5cc5d8bc0ec39b74dbd7f2564d0d8b423f347f86
Jun <1356675512@qq.com>
2024-02-21 14:03:34 +0800

    ui: 更新用户卡片UI样式

---
+++ src/views/home/components/userCard.vue
@@ -0,0 +1,79 @@
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue'
+import { userInfo } from '@/service/home'
+
+const avatarUrl = ref<string>('')
+const getUserInfo = () => {
+  userInfo().then(res => {
+    const { userPic } = res.data
+    avatarUrl.value = userPic
+  })
+}
+onMounted(() => {
+  getUserInfo()
+})
+</script>
+<template>
+  <el-row>
+    <el-col :span="8"></el-col>
+    <el-col :span="16">
+      <div class="user-card-container">
+        <div flex pr content-center w-full>
+          <el-avatar :size="50" :src="avatarUrl"></el-avatar>
+          <div class="user-profile">
+            <div font-600 class="user-card-name">用户名</div>
+            <div w-full>
+              <span class="user-card-tag">用户tag</span>
+            </div>
+          </div>
+        </div>
+        <div pt-6 flex justify-between>
+          <div flex flex-col items-center>
+            <span font-600>44</span>
+            <span pt2 color="#9499a0" font-size="3.2">关注</span>
+          </div>
+          <div flex flex-col items-center>
+            <span font-600>44</span>
+            <span pt2 color="#9499a0" font-size="3.2">粉丝</span>
+          </div>
+          <div flex flex-col items-center>
+            <span font-600>44</span>
+            <span pt2 color="#9499a0" font-size="3.2">文章</span>
+          </div>
+        </div>
+      </div>
+    </el-col>
+  </el-row>
+</template>
+<style lang="scss" scoped>
+.user-card-container {
+  width: 100%;
+  height: 144px;
+  background: #fff;
+  border-radius: 5px;
+  padding: 20px 16px;
+  color: var(--font-color-main);
+  box-sizing: border-box;
+
+  .user-profile {
+    padding-left: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+
+    .user-card-name {
+      font-size: 15px;
+      color: var(--font-color-user-name);
+    }
+
+    .user-card-tag {
+      display: inline-block;
+      font-size: 12px;
+      padding: 4px;
+      border-radius: 4px;
+      background: var(--font-color-user-name);
+      color: #fff;
+    }
+  }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,28 +1,61 @@
 <script lang='ts' setup>
 import Header from '@/views/home/components/header.vue'
+import UserCard from '@/views/home/components/userCard.vue'
+
 </script>
 <template>
-  <Header></Header>
-  <el-row :gutter="0">
-    <el-col :xs="6" :sm="6" :md="6" :lg="6" :xl="6"><div class="grid-content bg-purple"></div></el-col>
-    <el-col :xs="12" :sm="12" :md="12" :lg="12" :xl="12"><div class="grid-content bg-purple-light"></div></el-col>
-    <el-col :xs="6" :sm="6" :md="6" :lg="6" :xl="6"><div class="grid-content bg-purple"></div></el-col>
-  </el-row>
+  <div class="home-container-wrapper" h-full>
+    <Header />
+    <div class="home-wrapper" h-full>
+      <el-row :gutter="10" pt-2>
+        <el-col :xs="6" :sm="6" :md="6" :lg="6" :xl="6">
+          <UserCard />
+        </el-col>
+        <el-col :xs="12" :sm="12" :md="12" :lg="12" :xl="12">
+          <div class="grid-content bg-purple-light"></div>
+        </el-col>
+        <el-col :xs="6" :sm="6" :md="6" :lg="6" :xl="6">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+      </el-row>
+    </div>
+  </div>
 </template>
 <style lang='scss' scoped>
+.home-container-wrapper {
+  width: 100%;
+  height: 100%;
+  .home-wrapper {
+    width: 100%;
+    height: 100%;
+    box-sizing: border-box;
+    padding: 0 10px;
+    background-image: url('https://s1.hdslb.com/bfs/static/stone-free/dyn-home/assets/bg.png@1c.avif');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: bottom;
+  }
+}
+
+
 .el-col {
   border-radius: 4px;
 }
+
 .bg-purple-dark {
   background: #99a9bf;
 }
+
 .bg-purple {
   background: #d3dce6;
   height: 800px;
 }
+
 .bg-purple-light {
   background: #e5e9f2;
+  height: 1000px;
 }
+
 .grid-content {
   border-radius: 4px;
   min-height: 50px;
